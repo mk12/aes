844dc380a8b00a9f92271bf3e8f11ce08b84a114
Mitchell Kember <mk12360@gmail.com>
2012-12-09 00:01:08 -0500

    Use text field for ciphertext instead of URL
    It didn't even work when published because of the "same origin policy"
    which prevents XMLHttpRequests from making requests to other servers. I
    changed it so that the first text field lets you input the Base64
    ciphertext (copy and paste it) instead of using a URL.

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 AES Decryption
 ==============
 
-AES Decryption is a (surprise!) tool that decrypts files encrypted with the Advanced Encryption Standard. More specifically, it likes files that use aes-256-cbc and that are encoded in Base64. It is also very AESthetically pleasing.
+AES Descryption is an AESthetically pleasing web app which decrypts AES ciphertext. More specifically, it likes aes-256-cbc and Base64. The actual decryption is done on the client side using [CryptoJS][].
 
-The actual decryption is done on the client side using [CryptoJS][].
+This was more of an exercise in web design than anything else, which is why it might appear to be slightly useless.
 
 [CryptoJS]: https://code.google.com/p/crypto-js/
 
 Usage
 -----
 
-Your best bet to create a file that will work with AES Descryption is OpenSSL:
+Your best bet is OpenSSL: 
 
     $ openssl aes-256-cbc -base64 -in secrets -out secrets.aes
 
-Use a strong [passphrase][pass] and then store this file somewhere on the web (e.g. Dropbox). Fire up [AES Decryption][aes], enter in the URL and passhrase, and click the decrypt button.
+Use a strong [passphrase][pass] and then copy the contents of the file to your clipboard. Fire up [AES Decryption][aes], paste in the ciphertext, enter the passhrase, and click the decrypt button.
 
 [pass]: http://passphra.se
 [aes]: http://mitchellkember.com/aes/
@@ -24,6 +24,6 @@ License
 
 Copyright © 2012 Mitchell Kember
 
-AES Decryption is available under the MIT license, see [LICENSE][] for details.
+AES Decryption is available under the MIT license; see [LICENSE][] for details.
 
 [LICENSE]: https://github.com/mk12/aes/blob/gh-pages/LICENSE.md
--- js/script.js
+++ js/script.js
@@ -1,57 +1,29 @@
-function downloadUrl(url, callback) {
-    var request = window.ActiveXObject ?
-        new ActiveXObject('Microsoft.XMLHTTP') :
-        new XMLHttpRequest;
-    request.open('GET', url, true);
-    request.onload = callback;
-    request.send();
-}
-
-// Taken from the Jquery Validation plugin
-function validateUrl(str) {
-    return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(str);
-}
-
 function submit() {
-    var urlField = document.getElementById("url");
-    var passField = document.getElementById("pass");
+    var cipherField = document.getElementById("ciphertext");
+    var passField = document.getElementById("passphrase");
     var txtArea = document.getElementById("plaintext");
     txtArea.value = "";
 
-    var url = urlField.value;
-    var pass = passField.value;
+    var ciphertext = cipherField.value;
+    var passphrase = passField.value;
 
-    urlField.className = url ? "" : "error";
-    passField.className = pass ? "" : "error";
-    if (!url || !pass)
+    cipherField.className = ciphertext ? "" : "error";
+    passField.className = passphrase ? "" : "error";
+    if (!ciphertext || !passphrase)
         return;
-
-    if (!validateUrl(url))
-        url = "http://" + url;
-    if (!validateUrl(url)) {
-        txtArea.className = "error";
-        txtArea.value = "Error: Invalid URL.";
-        return;
-    }
 
     txtArea.className = "";
     txtArea.value = ". . .";
 
-    downloadUrl(url, function(e) {
-        if (this.status == 200) {
-            var decrypted = CryptoJS.AES.decrypt(this.responseText, pass);
-            var str = decrypted.toString(CryptoJS.enc.Utf8);
-            if (str) {
-                txtArea.value = str;
-            } else {
-                txtArea.className = "error";
-                txtArea.value = "Error: Wrong Password";
-            }
-        } else {
-            txtArea.className = "error";
-            txtArea.value = "Error: " + this.status + " " + this.statusText;
-        }
-    });
+    var decrypted = CryptoJS.AES.decrypt(ciphertext, passphrase);
+    try {
+        var str = decrypted.toString(CryptoJS.enc.Utf8);
+        if (!str) throw new Error();
+        txtArea.value = str;
+    } catch (e) {
+        txtArea.className = "error";
+        txtArea.value = "Error: Wrong Password";
+    }
 };
 
 window.onload = function() {
