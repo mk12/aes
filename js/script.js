856043ad51445f8fc870edbb1ae490483aa95629
Mitchell Kember <mk12360@gmail.com>
2013-07-20 15:42:01 -0400

    Tabs for indentation, spaces for alignment.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 function submit() {
-    var cipherField = document.getElementById("ciphertext");
-    var passField = document.getElementById("passphrase");
-    var txtArea = document.getElementById("plaintext");
-    txtArea.value = "";
+	var cipherField = document.getElementById("ciphertext");
+	var passField = document.getElementById("passphrase");
+	var txtArea = document.getElementById("plaintext");
+	txtArea.value = "";
 
-    var ciphertext = cipherField.value;
-    var passphrase = passField.value;
+	var ciphertext = cipherField.value;
+	var passphrase = passField.value;
 
-    cipherField.className = ciphertext ? "" : "error";
-    passField.className = passphrase ? "" : "error";
-    if (!ciphertext || !passphrase)
-        return;
+	cipherField.className = ciphertext ? "" : "error";
+	passField.className = passphrase ? "" : "error";
+	if (!ciphertext || !passphrase)
+		return;
 
-    txtArea.className = "";
-    txtArea.value = ". . .";
+	txtArea.className = "";
+	txtArea.value = ". . .";
 
-    var decrypted = CryptoJS.AES.decrypt(ciphertext, passphrase);
-    try {
-        var str = decrypted.toString(CryptoJS.enc.Utf8);
-        if (!str) throw new Error();
-        txtArea.value = str;
-    } catch (e) {
-        txtArea.className = "error";
-        txtArea.value = "Error: Wrong Password";
-    }
+	var decrypted = CryptoJS.AES.decrypt(ciphertext, passphrase);
+	try {
+		var str = decrypted.toString(CryptoJS.enc.Utf8);
+		if (!str) throw new Error();
+		txtArea.value = str;
+	} catch (e) {
+		txtArea.className = "error";
+		txtArea.value = "Error: Wrong Password";
+	}
 };
 
 window.onload = function() {
-    document.getElementById("form").onsubmit = function() {
-        submit();
-        return false;
-    };
+	document.getElementById("form").onsubmit = function() {
+		submit();
+		return false;
+	};
 };
 
